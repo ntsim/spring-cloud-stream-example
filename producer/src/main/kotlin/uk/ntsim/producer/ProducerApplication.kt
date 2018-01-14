a25238f176ac01d4a0961bc207275927561cb8bc
Nicholas Tsim <nicholastsim@gmail.com>
2018-01-14 22:53:53 +0000

    Add relevant docblocks to Producer application code

--- producer/src/main/kotlin/uk/ntsim/producer/ProducerApplication.kt
+++ producer/src/main/kotlin/uk/ntsim/producer/ProducerApplication.kt
@@ -19,12 +19,20 @@ fun main(args: Array<String>) {
 
   val userMessageSender = context.getBean(UserMessageSender::class.java)
   userMessageSender.sendUserMessage(UserMessage("test@test.com", firstName = "John", lastName = "Doe", age = 20))
+  userMessageSender.sendUserMessageDirectly(UserMessage("test2@test.com", firstName = "Melissa", lastName = "Stewart", age = 40))
 
   val addressMessageSender = context.getBean(AddressMessageSender::class.java)
   addressMessageSender.sendAddressMessage(AddressMessage("1 Sheffield Way", "City Center", "Sheffield", "S1 SHF"))
   addressMessageSender.sendAddressMessage(AddressMessage("X", "X", "X", "XXX"))
 }
 
+/**
+ * Spring Cloud Stream requires us to bind [MessageChannel]s in the
+ * producing application. These channels are automatically generated
+ * from the interface, so we do not need to be concerned about their
+ * implementation. This is the useful abstraction that decouples us
+ * from the messaging implementation i.e. RabbitMQ or Kafka.
+ */
 @Component
 interface ChannelBindings {
 
--- producer/src/main/kotlin/uk/ntsim/producer/Senders.kt
+++ producer/src/main/kotlin/uk/ntsim/producer/Senders.kt
@@ -2,22 +2,33 @@ package uk.ntsim.producer
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.cloud.stream.annotation.Output
 import org.springframework.cloud.stream.reactive.FluxSender
 import org.springframework.cloud.stream.reactive.StreamEmitter
-import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.MessageChannel
 import org.springframework.messaging.support.MessageBuilder
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
-import uk.ntsim.AddressMessage
-import uk.ntsim.PAYMENT_CHANNEL
-import uk.ntsim.PaymentMessage
-import uk.ntsim.UserMessage
+import uk.ntsim.*
 import java.math.BigDecimal
 import java.time.Duration
 
+/**
+ * 1st example - Sending messages using channel bindings.
+ *
+ * We can either inject in all of the Channels via the [ChannelBindings]
+ * or directly inject in the required channel using the [Qualifier] annotation.
+ *
+ * Sending messages through these channels is quite simple as long as the
+ * message is serializable. With Kotlin, this is pretty straightforward
+ * using data classes.
+ */
 @Component
-final class UserMessageSender(private val bindings: ChannelBindings) {
+final class UserMessageSender(
+  private val bindings: ChannelBindings,
+  @Qualifier(USER_CHANNEL) private val channel: MessageChannel
+) {
 
   private val log: Logger = LoggerFactory.getLogger(this::class.java)
 
@@ -25,8 +36,18 @@ final class UserMessageSender(private val bindings: ChannelBindings) {
     bindings.users().send(MessageBuilder.withPayload(message).build())
     log.info("Sent User message: $message")
   }
+
+  fun sendUserMessageDirectly(message: UserMessage) {
+    channel.send(MessageBuilder.withPayload(message).build())
+    log.info("Sent User message directly with channel: $message")
+  }
 }
 
+/**
+ * 2nd example - Sending messages through Processors/Transformers.
+ *
+ * Refer to the Consumer application's [AddressMessage] handler.
+ */
 @Component
 final class AddressMessageSender(private val bindings: ChannelBindings) {
 
@@ -38,6 +59,21 @@ final class AddressMessageSender(private val bindings: ChannelBindings) {
   }
 }
 
+/**
+ * 3rd example - Using Reactor for reactive programming.
+ *
+ * Spring Cloud Stream supports the Reactor project, allowing us to use
+ * reactive stream APIs. This might not necessarily be required in all
+ * instances, but can be particularly useful for things like
+ * long-running asynchronous tasks.
+ *
+ * Reactor provides a [Flux] primitive, which represents a stream of
+ * 1..N items, which we can do things such as [Flux.map], [Flux.filter]
+ * (as you might find in the Java 8 Streams API).
+ *
+ * Spring Cloud Stream itself then provides the mechanism to output this
+ * Flux via a [FluxSender].
+ */
 @Component
 final class PaymentMessageSender {
 
